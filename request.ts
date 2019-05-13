03eabbdd9e27484bf180961bda3ee87140960d0e
chwech <804699297@qq.com>
2019-05-13 15:00:06 +0800

    其它方法完善

--- request.ts
+++ request.ts
@@ -5,47 +5,57 @@
 interface PlainObject {
   [propName: string]: any;
 }
+// 类型别名, 这样就可以很方便使用和复用啦， 联合类型比较常用
+type data = PlainObject | URLSearchParams | string | ArrayBuffer | ArrayBufferView | FormData | File | Blob;
 interface Config {
   url: string;
   baseUrl?: string;
   method?: string; // 名字后加问号表示可选属性
   params?: PlainObject | URLSearchParams; // params是一个无格式对象(plain object)或 URLSearchParams 对象
+  responseType?: string; // `responseType` 表示服务器响应的数据类型，可以是 'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'
+  data?: data;
+  transformRequest?: Array<Function>;
+}
+// 响应结构
+interface ResponseObject {
+  data: any;
+  status: number;
+  statusText: string;
+  headers: Object;
+  config: Config;
 }
 // 默认配置对象
 let defaultConfig: Config = {
   url: '',
+  baseUrl: '',
   method: 'GET',
   params: {},
-  baseUrl: ''
+  responseType: 'json',
+  data: {},
+  transformRequest: [
+    function (data) {
+      return data
+    }
+  ]
 }
-
+function transformRequest (config: Config, data: data): data {
+  let temp: data = data
+  if (config.transformRequest) {
+    for (let fn of config.transformRequest) {
+      temp = fn(temp)
+    }
+  }
+  return temp
+}
 class Fetch {
   // 公有实例属性
-  public defaults: Config = defaultConfig;
+  public defaults: Config = defaultConfig; // 实例的默认配置
   // 静态属性
   static defaults: Config = defaultConfig; // 全局的默认配置
 
   constructor (opts: Config = defaultConfig) {
     // 实例属性赋值
     this.defaults.baseUrl = opts.baseUrl
-  }
-  public request (config: Config = defaultConfig) {
-    let sendUrl: string = this.defaults.baseUrl + config.url
-    let init = {
-      method: config.method
-    }
-    fetch(sendUrl, init)
-      .then(function(response) {
-        return response.json();
-      })
-  }
-  public get (url: string, config: Config = defaultConfig) {
-    let sendConfig: Config = this.mergeConfig({ ...config, url })
-    this.request(sendConfig)
-  }
-
-  public post () {
-    
   }
   // protected修饰的属性或方法，可以在类与子类中访问
   // 合并配置对象
@@ -58,6 +68,54 @@ class Fetch {
     }
     return sendConfig
   }
+  // 对外接口 start==========================================================================
+  public request (config: Config = defaultConfig) {
+    let sendUrl: string = this.defaults.baseUrl + config.url
+    let init = {
+      method: config.method,
+      // body: config.data
+    }
+    return fetch(sendUrl, init)
+      .then(function(response) {
+        if (config.responseType === 'json') {
+          return {
+            data: response.json(),
+            status: response.status,
+            headers: response.headers,
+            statusText: response.statusText,
+            config: config
+          }
+        }
+      })
+  }
+  public get (url: string, config: Config = defaultConfig) {
+    let sendConfig: Config = this.mergeConfig({ ...config, url })
+    return this.request(sendConfig)
+  }
+  public delete (url: string, config: Config = defaultConfig) {
+    let sendConfig: Config = this.mergeConfig({ ...config, url, method: 'DELETE' })
+    return this.request(sendConfig)
+  }
+  public head (url: string, config: Config = defaultConfig) {
+    let sendConfig: Config = this.mergeConfig({ ...config, url, method: 'HEAD' })
+    return this.request(sendConfig)
+  }
+  public put (url: string, data: data, config: Config = defaultConfig) {
+    data = transformRequest(config, data) // 转换数据
+    let sendConfig: Config = this.mergeConfig({ ...config, url, method: 'PUT', data })
+    return this.request(sendConfig)
+  }
+  public patch (url: string, data: data, config: Config = defaultConfig) {
+    data = transformRequest(config, data) // 转换数据
+    let sendConfig: Config = this.mergeConfig({ ...config, url, method: 'PATCH', data })
+    return this.request(sendConfig)
+  }
+  public post (url: string, data: data, config: Config = defaultConfig) {
+    data = transformRequest(config, data) // 转换数据
+    let sendConfig: Config = this.mergeConfig({ ...config, url, method: 'POST', data })
+    return this.request(sendConfig)
+  }
+  // 对外接口 end ==========================================================================
 }
 
 let axios = new Fetch()
