94fb1b3bac544e7e555970ad48e432b7add6f795
chwech <804699297@qq.com>
2019-05-29 10:49:56 +0800

    test:
    使用jest-fetch-mock来模拟请求

--- test/request.test.js
+++ test/request.test.js
@@ -1,12 +1,29 @@
 const fetchClass = require("../static/request.js").default
-jest.mock('node-fetch')
-test('测试get请求并返回正确的data', () => {
-  const axios = new fetchClass()
-  const resp = {
-    json: jest.fn(() => Promise.resolve({a: 1, b: 2}))
-  };
-  fetch.mockResolvedValue(resp);
-  return axios.get('/test').then(response => {
-    expect(response.data).toEqual({a: 1, b: 2})
+
+describe('测试get请求', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
   })
-});
+  test('请求并返回正确的data', () => {
+    fetch.mockResponseOnce(JSON.stringify({a: 1, b: 2}))
+
+    const axios = new fetchClass()
+    return axios.get('/test').then(response => {
+      expect(response.data).toEqual({a: 1, b: 2})
+    })
+  })
+
+  test('传params并返回正确的data', () => {
+    fetch.mockResponseOnce(JSON.stringify({a: 1, b: 2}))
+
+    const axios = new fetchClass()
+    return axios.get('/test', {
+      params: { a: 1 }
+    }).then(response => {
+      expect(response.data).toEqual({a: 1, b: 2})
+      //assert on the times called and arguments given to fetch
+      expect(fetch.mock.calls.length).toEqual(1)
+      expect(fetch.mock.calls[0][0]).toEqual('/test?a=1')
+    })
+  })
+})
